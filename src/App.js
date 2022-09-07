5d999e48f86d749f733e74406653ead7631f8c08
codessi <johansuh@gmail.com>
2022-09-07 15:34:37 -0700

    adjust img and text portion

--- src/App.js
+++ src/App.js
@@ -16,21 +16,39 @@ function App() {
       </div>
 
       {/* info intro */}
-      <div className="w-full  h-screen p-4  flex flex-col items-center justify-center bg-teal-500 text-white ">
+      <div className="w-full   p-4  flex flex-col items-center justify-start bg-teal-500 text-white ">
         <h3 className="text-4xl font-bold my-5">ABOUT ME</h3>
-        <div className="flex flex-col max-w-lg gap-8 lg:flex-row lg:max-w-5xl ">
-          <img class="shadow-xl w-[32rem]" src={profile} alt="" />
-          <p>
-            As a React developer trained in General Assembly, I enjoy computer
-            programming as crafts like woodworking or pottery: design plan &
-            patiently improve the product, seeing it to grow to something I made,
-            something I am proud of. Here are some values I hold on to when I
-            work. I choose to do hard things. I strive to be incredibly patient. I
-            love efficiency but not shortcut. I welcome new idea but approach
-            minimalistically. consistent slow changes are good. I am adaptable
-            because I enjoy learning. I use habits as task automations.
-          </p>
+        <div className="flex justify-center items-center flex-col max-w-lg gap-8 lg:flex-row lg:max-w-5xl ">
+          <div className="lg:w-1/2">
+            <img className="shadow-xl  w-full" src={profile} alt="" />
+          </div>
+          {/* text-container  */}
+          <div className="lg:w-1/2 font-thin text-xl">
+            <p>
+              As a React developer trained in General Assembly, I enjoy computer
+              programming as crafts like woodworking or pottery: design plan &
+              patiently improve the product, seeing it to grow to something I
+              made, something I am proud of.
+            </p>
+            <br />
+            <p>Here are some values I hold on to when I work.</p>
+            <br />
+            <ul>
+              <li>I choose to do hard things.</li>
+              <li>I strive to be incredibly patient.</li>
+              <li>I love efficiency but not shortcut.</li>
+              <li>I welcome new idea but approach minimalistically.</li>
+              <li>consistent slow changes are good.</li>
+              <li> I am adaptable because I enjoy learning.</li>
+              <li>I use habits as task automations.</li>
+            </ul>
+          </div>
         </div>
+      </div>
+
+      {/* tech stack  */}
+      <div className="">
+
       </div>
     </div>
   );
